94c54441432835e74664197ae119606034b77ea9
mykola-myhailov <mykola.myhailov@gmail.com>
2024-05-22 20:03:53 +0300

    base structure

---
+++ lib/utils/logger.dart
@@ -0,0 +1,33 @@
+
+import 'package:logger/logger.dart';
+
+class Logger {
+  static final _logger = Logger(
+     printer: PrettyPrinter(
+        methodCount: 2,
+        errorMethodCount: 8,
+        lineLength: 120,
+        colors: true,
+        printEmojis: true,
+        printTime: true,
+      ),
+      filter: DevelopmentFilter(),
+      output: ConsoleOutput(),
+  );
+
+  static void d(String message) {
+    _logger.d(message);
+  }
+
+  static void e(String message) {
+    _logger.e(message);
+  }
+
+  static void w(String message) {
+    _logger.w(message);
+  }
+
+  static void i(String message) {
+    _logger.i(message);
+  }
+}
